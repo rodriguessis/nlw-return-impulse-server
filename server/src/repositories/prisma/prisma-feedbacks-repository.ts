180891a60387883ef3affc870b92a49ddf05fb1e
Ricardo Ferreira <rodrigues.fricardo@gmail.com>
2022-05-07 08:36:10 -0300

    novo commit

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -3,7 +3,8 @@ import { FeedbackCreateData, FeedbackRepository } from "../feedbacks-repository"
 
 export class PrismaFeedbacksRepository implements FeedbackRepository {
     async create( { type, comment, screenshot } : FeedbackCreateData) {
-
+        
+        // esse codigo precisa ser configurado novamente 
         // await prisma.feedback.create({
         //     data : {
         //         type,
