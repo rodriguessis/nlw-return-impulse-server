9a96b05a66e18323c7999b8d704d1365a35395a7
Ricardo Ferreira <rodrigues.fricardo@gmail.com>
2022-05-07 10:45:45 -0300

    configurando a prota do banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json())
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('rodando na porta 3333')
 });
+//postgresql://postgres:ZB1Aa5cy0vdwZFn1JmRO@containers-us-west-47.railway.app:6302/railway
